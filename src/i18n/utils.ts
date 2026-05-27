a2592190d3d782931afeb9664e28b4ea6e94e07f
Valentina.Baranova <valentinab@bluetrailsoft.com>
2026-05-27 19:43:38 -0300

    Refine project section

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -7,7 +7,7 @@ export function getLangFromUrl(url: URL) {
 }
 
 export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof typeof ui[typeof defaultLang]) {
-    return ui[lang][key] || ui[defaultLang][key];
+  return function t<K extends keyof typeof ui[typeof defaultLang]>(key: K): typeof ui[typeof defaultLang][K] {
+    return (ui[lang] as any)[key] || (ui[defaultLang] as any)[key];
   }
 }
